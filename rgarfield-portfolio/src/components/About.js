8032911ce339d8a644265839f49fac2fc7a97f9b
Ross <rossgarfield@gmail.com>
2022-09-29 16:40:45 -0500

    contact links

--- rgarfield-portfolio/src/components/About.js
+++ rgarfield-portfolio/src/components/About.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import myPhoto from "../Profile.webp";
+import myPhoto from "../Profile.jpg";
 
 function About() {
   return (
     <div className="about">
-      <h2>About me</h2>
+      <h2>About</h2>
       <img src={myPhoto} alt="me" className="about-photo" />
       <div className="about-description">
-        <p>Before becoming a software engineer, I earned a Bachelor of Science in Architecture from Tulane University</p>
-        <p>At this time, I am open to any junior, internship, or apprenticeship opportunities in frontend developement and software engineering.</p>
+        <p>Before becoming a software engineer, I earned a Bachelor of Science in Architecture from Tulane University. I then pursued a passion in cooking, working for three years in one of New Orleans' most renowned restaurant groups. Seeking a new challenge, I enrolled in the Flatiron School software engineering bootcamp. There, I continued my penchant for problem-solving and design by creating and collaborating on compelling web applications. Today, I am continuing my growth as a web developer, seeking to solve problems we encounter every day.</p>
+        <p>At this time, I am open to any junior, internship, or apprenticeship opportunities in frontend, backend, or full-stack developement and software engineering.</p>
       </div>
     </div>
   )
--- rgarfield-portfolio/src/components/Contact.js
+++ rgarfield-portfolio/src/components/Contact.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip';
+import { BsFillEnvelopeFill, BsFillFileEarmarkTextFill, BsLinkedin, BsGithub } from "react-icons/bs";
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <h2>Contact me</h2>
+      <a className="contact-option" data-tip data-for="email" target="_blank" href="mailto:rossgarfield@gmail.com">
+        <BsFillEnvelopeFill/>
+      </a>
+      <a className="contact-option" data-tip data-for="resume" target="_blank" href="https://docs.google.com/document/d/1OOSamrcFre-r7etsvDcOWUdiq7lpV7dZ0Jlvmi4ES_o/edit">
+        <BsFillFileEarmarkTextFill/>
+      </a>
+      <a className="contact-option" data-tip data-for="linkedin" target="_blank"  href="https://www.linkedin.com/in/rossgarfield/">
+        <BsLinkedin/>
+      </a>
+      <a className="contact-option" data-tip data-for="github" target="_blank"  href="https://github.com/rgarfield11">
+        <BsGithub/>
+      </a>
+
+      <ReactTooltip id='email' type='light' effect='solid'>
+        <span>Email Me</span>
+      </ReactTooltip>
+
+      <ReactTooltip id='resume' type='light' effect='solid'>
+        <span>My Resume</span>
+      </ReactTooltip>
+
+      <ReactTooltip id='linkedin' type='light' effect='solid'>
+        <span>My LinkedIn</span>
+      </ReactTooltip>
+
+      <ReactTooltip id='github' type='light' effect='solid'>
+        <span>My GitHub</span>
+      </ReactTooltip>
+    </div>
   )
 }
 
